74b431ee452f6075acd5945f7d84f09daf4455d2
Ruiyotabby <ruiyotabby@gmail.com>
2022-09-13 18:20:53 +0300

    Add a function which takes input from user

--- script.js
+++ script.js
@@ -11,6 +11,42 @@ for(let b of btn){
     b.addEventListener('click', (e) => {
         display.textContent += e.target.textContent;
 
+        if(sign == '') {
+            num1 = getFirst(num1, e);
+        } else {
+            num2 = getFirst(num2, e);
+        }
         
     })
 }
+
+
+
+let getInput = (num, event) => {
+    
+    for(let p of operand){
+        while(event.target == p){
+            num += event.target.textContent;
+            console.log('bjb');
+            break;
+        }
+    }
+    for(let o of operator){
+        while(event.target == o){
+            sign += event.target.textContent;
+            console.log('qweer');
+            break;
+        }
+    }
+    return num;
+}
+
+// let getSecond = (event) => {
+//     for(let p of operand){
+//         while(event.target == p){
+//             num2 += event.target.textContent;
+//             console.log('sdd');
+//             break;
+//         }
+//     }
+// }
